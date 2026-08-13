6918c9110cb212460b553a5e57a7599f8d9e5e12
gggoooo6666 <Eclosionfairy@outlook.com>
2026-08-13 17:13:02 +0800

    upload: app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import RecruitmentDashboard from "./recruitment-dashboard";
+
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  return <RecruitmentDashboard />;
+}
